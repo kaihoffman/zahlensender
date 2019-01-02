8d67a9b504bade3d46a23c3bb985c75074576021
Kai Hoffman <notkaiho@gmail.com>
2019-01-02 14:49:19 +0000

    Minor fixes, ready to be deployed on Heroku scheduler. Onto the next thing

--- zahlensender.rb
+++ zahlensender.rb
@@ -1,9 +1,8 @@
-# Zahlensender will (eventually) be a Twitter bot that posts random strings
-# of numbers to Twitter at random intervals, just like a real Numbers Station
-# would do.
-# Eventually, this bot will aim to encode real tweets from real accounts to
-# post in code. The idea is mostly to teach myself the Twitter API, Ruby program
-# deployment and oAuth.
+# Zahlensender will (eventually) be a Twitter bot that posts random
+# strings of numbers to Twitter at random intervals, just like a real
+# Numbers Station would do. Eventually, this bot will aim to encode real
+# tweets from real accounts to post in code. The idea is mostly to teach
+# myself the Twitter API, Ruby program deployment and oAuth.
 
 # Zahlensender will generate a Tweet's worth of numbers in 5-character sets
 # All preceded by the string "ACHTUNG ACHTUNG" and followed by "ENDE ENDE" in a
@@ -19,10 +18,10 @@
 
 require 'twitter'
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "YOUR_CONSUMER_KEY"
-  config.consumer_secret     = "YOUR_CONSUMER_SECRET"
-  config.access_token        = "YOUR_ACCESS_TOKEN"
-  config.access_token_secret = "YOUR_SECRET_TOKEN"
+  config.consumer_key        = "YOUR CODE HERE"
+  config.consumer_secret     = "YOUR CODE HERE"
+  config.access_token        = "YOUR CODE HERE"
+  config.access_token_secret = "YOUR CODE HERE"
 end
 
 # Generate the tweet payload
@@ -52,6 +51,6 @@ end
 # client.update("")
 create_array
 print_header
-puts print_numbers
+print_numbers
 print_footer
 client.update(print_header + "\n\n" + print_numbers + "\n" + print_footer)

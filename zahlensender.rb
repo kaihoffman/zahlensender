72c75fef425c9d0851bd443be18f93bb5bf7a369
Kai <kai@civo.com>
2019-07-15 12:24:35 +0100

    Fix rubocop whines

--- zahlensender.rb
+++ zahlensender.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # Zahlensender will (eventually) be a Twitter bot that posts random
 # strings of numbers to Twitter at random intervals, just like a real
 # Numbers Station would do. Eventually, this bot will aim to encode real
@@ -12,43 +14,42 @@
 # Uses the Ruby Twitter gem - sends tweets using:
 # client.update("Tweet text here")
 
-#!/usr/bin/ruby
+# !/usr/bin/ruby
 
 # Configure Twitter API client data by editing in the keys below
 
 require 'twitter'
 client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = "YOUR CODE HERE"
-  config.consumer_secret     = "YOUR CODE HERE"
-  config.access_token        = "YOUR CODE HERE"
-  config.access_token_secret = "YOUR CODE HERE"
+  config.consumer_key        = 'YOUR CODE HERE'
+  config.consumer_secret     = 'YOUR CODE HERE'
+  config.access_token        = 'YOUR CODE HERE'
+  config.access_token_secret = 'YOUR CODE HERE'
 end
 
 # Generate the tweet payload
 def create_array
-  @payload = 60.times.map{ rand(10) }
+  @payload = 60.times.map { rand(10) }
 end
 
 def print_header
-  "ACHTUNG ACHTUNG"
+  'ACHTUNG ACHTUNG'
 end
 
 def print_footer
-  "ENDE ENDE"
+  'ENDE ENDE'
 end
 
 def print_numbers
   index = 0
-  returnstring = ""
+  returnstring = ''
   12.times do
-    returnstring.concat(@payload[index..(index + 4)].join("  ") + "\n")
+    returnstring.concat(@payload[index..(index + 4)].join(' ') + "\n")
     index += 5
   end
   returnstring
 end
 
 # Send tweet (in test, print to console)
-# client.update("")
 create_array
 print_header
 print_numbers
